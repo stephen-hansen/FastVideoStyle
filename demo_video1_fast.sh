cc1b808810be18a628069beae97b859c6271d119
Stephen Hansen <sph77@drexel.edu>
2020-05-28 00:51:18 -0400

    change style

--- demo_video1_fast.sh
+++ demo_video1_fast.sh
@@ -6,7 +6,7 @@ cd videos;
 curl https://www.cs.drexel.edu/~sph77/CS583/video1.mp4 > video1.mp4
 # Consider installing ffmpeg, reducing video size here
 cd ../images;
-axel -n 1 https://vignette.wikia.nocookie.net/strangerthings8338/images/e/e0/Wiki-background.jpeg/revision/latest?cb=20170522192233 --output=style1.png;
+axel -n 1 https://www.cs.drexel.edu/~sph77/CS583/rooftop.jpg --output=style1.png;
 convert -resize 50% style1.png style1.png;
 cd ..;
 python video_demo.py --fast --content_video_path videos/video1.mp4 --style_image_path images/style1.png --output_video_path results/demo_result_video1.avi;
