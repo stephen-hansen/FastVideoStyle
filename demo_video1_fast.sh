6de711eba6cc5d0efb3e221b4981b05f6a09d97e
Stephen Hansen <sph77@drexel.edu>
2020-05-28 01:10:49 -0400

    trying another style

--- demo_video1_fast.sh
+++ demo_video1_fast.sh
@@ -6,7 +6,7 @@ cd videos;
 curl https://www.cs.drexel.edu/~sph77/CS583/theroom.mp4 > video1.mp4
 # Consider installing ffmpeg, reducing video size here
 cd ../images;
-axel -n 1 https://www.cs.drexel.edu/~sph77/CS583/rooftop.jpg --output=style1.png;
+axel -n 1 https://www.cs.drexel.edu/~sph77/CS583/scream.jpg --output=style1.png;
 convert -resize 50% style1.png style1.png;
 cd ..;
 python video_demo.py --fast --content_video_path videos/video1.mp4 --style_image_path images/style1.png --output_video_path results/demo_result_video1.avi;
