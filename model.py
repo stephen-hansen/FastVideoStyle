68ed4346c8f939d8ad72f8cfcd4b90fbc0871125
Stephen Hansen <sph77@drexel.edu>
2020-06-05 14:03:07 -0400

    grad problems

--- model.py
+++ model.py
@@ -108,6 +108,9 @@ class TemporalLoss(nn.Module):
 
 cnn = models.vgg19(pretrained=True).features.to(device).eval()
 
+for param in cnn.parameters():
+    param.requires_grad_(False)
+
 normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
