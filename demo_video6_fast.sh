55dc69eeeb077fd8e7d2070185d69b6dac98ece9
Stephen Hansen <sph77@drexel.edu>
2020-06-06 21:06:37 -0400

    fix output video6

--- demo_video6_fast.sh
+++ demo_video6_fast.sh
@@ -1,7 +1,7 @@
 mkdir images -p && mkdir videos -p && mkdir results -p;
 rm videos/video1.mp4 -rf;
 rm images/style1.png -rf;
-rm results/demo_result_video5.avi
+rm results/demo_result_video6.avi
 cd videos;
 curl https://www.cs.drexel.edu/~sph77/CS583/field.mp4 > video1.mp4
 # Consider installing ffmpeg, reducing video size here
@@ -9,4 +9,4 @@ cd ../images;
 axel -n 1 https://www.cs.drexel.edu/~sph77/CS583/field.jpg --output=style1.png;
 # convert -resize 50% style1.png style1.png;
 cd ..;
-python video_demo.py --artistic_optical_flow --fast --nframes 10 --content_video_path videos/video1.mp4 --style_image_path images/style1.png --output_video_path results/demo_result_video5.avi;
+python video_demo.py --artistic_optical_flow --fast --nframes 10 --content_video_path videos/video1.mp4 --style_image_path images/style1.png --output_video_path results/demo_result_video6.avi;
